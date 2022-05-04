1592bc878e73a03f97f7a125cc11adb00321177d
Maksim Masalov <simba7@inbox.ru>
2022-05-04 23:33:24 +0300

    Code style fixes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Providers;
 
 use Illuminate\Cache\RateLimiting\Limit;
@@ -15,7 +17,6 @@ class RouteServiceProvider extends ServiceProvider
      *
      * This is used by Laravel authentication to redirect users after login.
      *
-     * @var string
      */
     public const HOME = '/home';
 
