9bb42383216eb743ba59842b0a6bd270f1998354
Maksim Masalov <simba7@inbox.ru>
2022-05-06 00:47:24 +0300

    Added base page component

--- resources/js/views/HomePage.vue
+++ resources/js/views/HomePage.vue
@@ -1,12 +1,17 @@
 <template>
-  <div>
-    Homepage
-  </div>
+  <page-component>
+    <div>
+      Homepage
+    </div>
+  </page-component>
 </template>
 
 <script lang="ts">
+import PageComponent from "@/components/PageComponent.vue";
+
 export default {
   name: "HomePage",
+  components: {PageComponent},
 }
 </script>
 
